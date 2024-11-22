8866a027a8c80465ea8d66c268ee110ffe327a1e
Lucy Costa <30415579+lucycosta@users.noreply.github.com>
2024-11-22 17:45:16 -0300

    Update leds.vhd
    padronização de dados

--- leds.vhd
+++ leds.vhd
@@ -5,7 +5,7 @@ use ieee.std_logic_1164.all;
 
 entity leds is
   port( N: in std_logic_vector (2 downto 0);
-        R, G, B: out bit);
+        R, G, B: out std_logic);
 end leds;
 
 architecture saida_leds of leds is
