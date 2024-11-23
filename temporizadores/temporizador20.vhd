4ed35d5f2b6c8fa7bea457b1a72b0ecc541de358
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2024-11-23 12:22:18 -0300

    refeat: diferenciar as variáveis

--- temporizadores/temporizador20.vhd
+++ temporizadores/temporizador20.vhd
@@ -2,20 +2,20 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity T20 is
-    Port ( clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           enable : in STD_LOGIC;
-           timeout : out STD_LOGIC);
+    Port ( clkT20 : in STD_LOGIC;
+           resetT20 : in STD_LOGIC;
+           enableT20 : in STD_LOGIC;
+           timeoutT20 : out STD_LOGIC);
 end T20;
 
 architecture ckt of T20 is
-    signal count : INTEGER := 0;
-    constant max_count : INTEGER := 20; -- 20 segundos para um clock de 1 Hz
+    signal countT20 : INTEGER := 0;
+    constant max_countT20 : INTEGER := 20; -- 20 segundos para um clock de 1 Hz
 begin
-    timeout <= '1' when count = max_count else '0';
+    timeoutT20 <= '1' when countT20 = max_countT20 else '0';
 
-    count <= 0 when reset = '1' else
-             count + 1 when rising_edge(clk) and enable = '1' and count < max_count else
-             0 when rising_edge(clk) and enable = '1' and count = max_count else
-             count;
+    countT20 <= 0 when resetT20 = '1' else
+             countT20 + 1 when rising_edge(clkT20) and enableT20 = '1' and countT20 < max_countT20 else
+             0 when rising_edge(clkT20) and enableT20 = '1' and countT20 = max_countT20 else
+             countT20;
 end ckt;
