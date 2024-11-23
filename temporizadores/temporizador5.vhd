a027ceee059b8229a0fbf6e0809dbca9b0708c8c
Rutileno Gabriel <92885893+BigLeno@users.noreply.github.com>
2024-11-23 12:22:36 -0300

    refeat: diferenciar as variáveis

--- temporizadores/temporizador5.vhd
+++ temporizadores/temporizador5.vhd
@@ -2,20 +2,20 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity T5 is
-    Port ( clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
-           enable : in STD_LOGIC;
-           timeout : out STD_LOGIC);
+    Port ( clkT5 : in STD_LOGIC;
+           resetT5 : in STD_LOGIC;
+           enableT5 : in STD_LOGIC;
+           timeoutT5 : out STD_LOGIC);
 end T5;
 
 architecture ckt of T5 is
-    signal count : INTEGER := 0;
-    constant max_count : INTEGER := 5; -- 5 segundos para um clock de 1 Hz
+    signal countT5 : INTEGER := 0;
+    constant max_countT5 : INTEGER := 5; -- 5 segundos para um clock de 1 Hz
 begin
-    timeout <= '1' when count = max_count else '0';
+    timeoutT5 <= '1' when countT5 = max_countT5 else '0';
 
-    count <= 0 when reset = '1' else
-             count + 1 when rising_edge(clk) and enable = '1' and count < max_count else
-             0 when rising_edge(clk) and enable = '1' and count = max_count else
-             count;
+    countT5 <= 0 when resetT5 = '1' else
+             countT5 + 1 when rising_edge(clkT5) and enableT5 = '1' and countT5 < max_countT5 else
+             0 when rising_edge(clkT5) and enableT5 = '1' and countT5 = max_countT5 else
+             countT5;
 end ckt;
